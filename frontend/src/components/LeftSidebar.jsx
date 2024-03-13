c0798e05aa1c646c4888699add190d7a71667e31
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2024-03-14 00:57:50 +0530

    Fixed some bugs and added responsiveness

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -3,17 +3,23 @@ import { AiFillHome } from "react-icons/ai";
 import { BsPersonHearts } from "react-icons/bs";
 import { FaBell, FaBook, FaCode, FaEnvelope, FaSearch } from "react-icons/fa";
 import { IoPerson } from "react-icons/io5";
+import { BsFeather } from "react-icons/bs";
 
 const LeftSidebar = () => {
   return (
-    <div className=" min-h-screen border-r-2">
-      <div className="p-2 rounded-full  hover:bg-gray-200 items-center flex-start justify-center px-3">
-        <img width={30} className="object-contain " src="/public/X.png" alt="" />
+    <div className="min-h-screen lg:border-r-2 ">
+      <div className="p-4 rounded-[50%] lg:rounded-full hover:bg-gray-100 items-center  lg:flex-start justify-center px-3">
+        <img
+          width={30}
+          className="object-contain "
+          src="/public/X.png"
+          alt=""
+        />
       </div>
 
       <div>
         <div className="flex gap-2 items-center my-2 p-4 hover:bg-gray-200 rounded-full">
-          <p className="text-2xl">
+          <p className="text-3xl">
             <AiFillHome />
           </p>
           <h1 className=" hidden lg:flex font-bold">Home</h1>
@@ -21,40 +27,40 @@ const LeftSidebar = () => {
       </div>
       <div>
         <div className="flex gap-2 items-center my-2 p-4 hover:bg-gray-200 rounded-full">
-          <p className="text-2xl">
-          <FaSearch/>
+          <p className="text-3xl">
+            <FaSearch />
           </p>
           <h1 className=" hidden lg:flex font-bold">Explore</h1>
         </div>
       </div>
       <div>
         <div className="flex gap-2 items-center my-2 p-4 hover:bg-gray-200 rounded-full">
-          <p className="text-2xl">
-          <FaBell />
+          <p className="text-3xl">
+            <FaBell />
           </p>
           <h1 className=" hidden lg:flex font-bold">Notifications</h1>
         </div>
       </div>
       <div>
         <div className="flex gap-2 items-center my-2 p-4 hover:bg-gray-200 rounded-full">
-          <p className="text-2xl">
-          <FaEnvelope />
+          <p className="text-3xl">
+            <FaEnvelope />
           </p>
           <h1 className=" hidden lg:flex font-bold">Messages</h1>
         </div>
       </div>
       <div>
         <div className="flex gap-2 items-center my-2 p-4 hover:bg-gray-200 rounded-full">
-          <p className="text-2xl">
-          <FaCode />
+          <p className="text-3xl">
+            <FaCode />
           </p>
           <h1 className=" hidden lg:flex font-bold">Grok</h1>
         </div>
       </div>
       <div>
         <div className="flex gap-2 items-center my-2 p-4 hover:bg-gray-200 rounded-full">
-          <p className="text-2xl">
-          <FaBook />
+          <p className="text-3xl">
+            <FaBook />
           </p>
           <h1 className=" hidden lg:flex font-bold">Lists</h1>
         </div>
@@ -62,8 +68,8 @@ const LeftSidebar = () => {
 
       <div>
         <div className="flex gap-2 items-center my-2 p-4 hover:bg-gray-200 rounded-full">
-          <p className="text-2xl">
-          <BsPersonHearts />
+          <p className="text-3xl">
+            <BsPersonHearts />
           </p>
           <h1 className=" hidden lg:flex font-bold">Communication</h1>
         </div>
@@ -71,20 +77,32 @@ const LeftSidebar = () => {
       <div>
         <div className="flex gap-2 items-center my-2 p-4 hover:bg-gray-200 rounded-full">
           <span className="rounded-full">
-            <img className="object-fill" width={25} src="/public/X.png" alt="" />
+            <img
+              className="object-fill"
+              width={25}
+              src="/public/X.png"
+              alt=""
+            />
           </span>
           <h1 className=" hidden lg:flex font-bold">Premium</h1>
         </div>
       </div>
       <div>
         <div className="flex gap-2 items-center my-2 p-4 hover:bg-gray-200 rounded-full">
-          <p className="text-2xl">
-          <IoPerson />
+          <p className="text-3xl">
+            <IoPerson />
           </p>
           <h1 className=" hidden lg:flex font-bold">Profile</h1>
         </div>
       </div>
-      <button className="bg-blue-400 flex items-center justify-center px-20 py-3 rounded-full hover:opacity-[0.8]">Post</button>
+      <div className="flex justify-center lg:justify-start rounded-full">
+        <span className="text-3xl lg:hidden hover:bg-blue-400 p-2 rounded-full">
+          <BsFeather />
+        </span>
+        <button className="hidden lg:flex  bg-blue-400  items-center justify-center px-20 py-3 rounded-full hover:opacity-[0.8] ">
+          Post
+        </button>
+      </div>
     </div>
   );
 };
